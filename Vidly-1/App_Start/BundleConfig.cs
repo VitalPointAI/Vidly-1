f7781a77f35dfd5f1a0fca375e3246f3c302bff6
Aaron Luhning <aaronluhning@hotmail.com>
2018-06-16 18:18:49 -0400

    initial

--- Vidly-1/App_Start/BundleConfig.cs
+++ Vidly-1/App_Start/BundleConfig.cs
@@ -24,8 +24,8 @@ namespace Vidly_1
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/bootstrap-lumen.css",
+                      "~/Content/Site.css"));
         }
     }
 }
